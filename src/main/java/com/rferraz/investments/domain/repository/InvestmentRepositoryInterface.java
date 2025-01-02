3e44801215994e4cc76031c3735bae4132213733
Ruan Ferraz <ruanlima155@gmail.com>
2025-01-02 11:55:40 -0300

    refact: remove InvestmentRepositoryInterface

--- src/main/java/com/rferraz/investments/domain/repository/InvestmentRepositoryInterface.java
+++
@@ -1,7 +0,0 @@
-package com.rferraz.investments.domain.repository;
-
-import com.rferraz.investments.domain.entities.Investment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvestmentRepositoryInterface extends JpaRepository<Investment, String> {
-}
